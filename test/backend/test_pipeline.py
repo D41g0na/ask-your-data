33d84cd4c1fae4b2625b879c3f95b9a2d3a79ee9
D41g0na <ahelme@wanadoo.fr>
2026-07-05 18:09:26 +0200

    test(backend): update file path

--- test/backend/test_pipeline.py
+++ test/backend/test_pipeline.py
@@ -1,10 +1,11 @@
 from backend.ingestion.pipeline import index_document
 
 import uuid
+from pathlib import Path
 
 def test_index_document():
     document_id = str(uuid.uuid4())
-    file_path = "/test/backend/test_extract.pdf"
+    file_path = Path(__file__).parent / "test_extract.pdf"
     metadata = {"author": "John Doe", "topic":"law"}
 
     nb_chunk = index_document(document_id, file_path)
