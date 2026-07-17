99e202290ddfb8475618c3b6ba1cccc69b9b46a5
D41g0na <ahelme@wanadoo.fr>
2026-07-17 15:02:10 +0200

    test: update fixture conftest

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,17 @@ import uuid
 import pytest
 
 from backend.database.repository_document import insert_document
+from backend.database.connection import get_connection
+from backend.database.schema import (
+    create_documents_table,
+    create_chunks_table,
+)
 
+
+@pytest.fixture(scope="session", autouse=True)
+def initialize_test_database():
+    create_documents_table()
+    create_chunks_table()
 
 @pytest.fixture
 def document_id():
@@ -16,4 +26,17 @@ def document_id():
         metadata={"author": "John Doe"},
     )
 
-    return document_id
+    yield document_id
+
+    conn = get_connection()
+
+    try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    " DELETE FROM documents WHERE document_id = %s",
+                    (document_id,),
+                )
+            conn.commit()
+
+    finally:
+        conn.close()
